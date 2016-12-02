989104e977562b1c1cc1683e2641d26e7b0b16b8
Patrick Egan <mrpatrickegan@gmail.com>
2016-12-02 00:24:26 +0000

    final commit

--- whoami/server.js
+++ whoami/server.js
@@ -1,7 +1,11 @@
 var express = require('express')
 var app = express()
 app.get('/', function(req,res) {
-    res.send('Hello World!')
+    var header = req.headers
+    var ipaddress = header["x-forwarded-for"];
+    var language = header["accept-language"].split(',')[0];
+    var software = header["user-agent"].match(/\((.+)\)/);
+    res.json({ipaddress: ipaddress, language: language, software: software[1]})
 })
 
 app.listen(8080, function() {
